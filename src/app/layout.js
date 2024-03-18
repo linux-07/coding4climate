0d5442b5b9c56a8469218647907959b16d42322e
arnavnagpurkar <arnavn.dev@gmail.com>
2024-03-18 14:43:11 +0530

    Footer Added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export const metadata = {
   title: "coding4climate | Arnav Nagpurkar",
@@ -12,6 +13,7 @@ export default function RootLayout({ children }) {
       <body className="dark:bg-zinc-950 dark:text-white bg-zinc-200 text-black">
         <Navbar />
         {children}
+        <Footer />
       </body>
     </html>
   );

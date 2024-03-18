5493d81af00729701d16982719f23c40eb53cf34
arnavnagpurkar <arnavn.dev@gmail.com>
2024-03-18 14:49:36 +0530

    Navbar bg change

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import Link from "next/link"
 const Footer = () => {
   return (
     <footer className="absolute bottom-0 w-full">
-      <div class="dark:bg-zinc-800 bg-slate-300 bg-opacity-75">
+      <div class="dark:bg-zinc-900 bg-slate-300 bg-opacity-75">
         <div class="container mx-auto py-4 px-5 flex flex-wrap flex-col sm:flex-row">
           <p class="dark:text-slate-300 text-sm text-center sm:text-left">© 2024 &nbsp;&nbsp; coding4climate —
             <Link href="https://github.com/arnavnagpurkar" class="dark:text-slate-400 dark:hover:text-slate-300 text-slate-800 hover:text-slate-950 transition-all ml-1" target="_blank">@arnavnagpurkar</Link>

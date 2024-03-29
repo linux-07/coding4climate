e9a8edfcad04ac34f40c898d933276c80d25579b
arnavnagpurkar <arnavn.dev@gmail.com>
2024-03-29 20:08:24 +0530

    Minor text changes

--- src/app/page.js
+++ src/app/page.js
@@ -7,19 +7,19 @@ export default function Home() {
     <main>
       <div className="container mx-1 md:mx-0">
         <div className="h-[75vh] w-[100vw] mx-auto flex justify-center items-center flex-col">
-          <h1 className="text-4xl md:text-7xl text-center w-full font-bold">
-            Let&apos;s solve environmental issues
+          <h1 className="text-4xl md:text-6xl text-center w-full font-bold">
+            Let's unite for a sustainable future
           </h1>
-          <p className="md:text-lg text-sm text-center w-full dark:text-gray-400 text-gray-500 mt-8 ">
-            Welcome to our platform dedicated to solving environmental challenges!
+          <p className="md:text-lg text-sm text-center w-full dark:text-zinc-300 text-zinc-800 mt-8 ">
+            Welcome to our platform dedicated to addressing environmental challenges!
             <br />
             Discover innovative solutions to pressing issues threatening our planet.
             <br />
             Explore curated problems and solutions below.
             <br />
-            Together, let&apos;s make a greener, healthier world!
+            Together, let's make a greener, healthier world!
             <br />
-            I&apos;ll try to solve the problems that come under the &quot;Land League&quot;
+            Dive into solutions for climate change and sustainable land use practices through the Land League.
             <br />
             #coding4climate
           </p>
@@ -27,7 +27,7 @@ export default function Home() {
             href="/landleague"
             className="md:px-10 px-7 md:py-8 py-5 font-semibold text-xl border dark:border-white border-black my-12 dark:bg-zinc-900 bg-slate-200 rounded-full dark:hover:bg-zinc-800 hover:bg-slate-300 transition-all"
           >
-            Land League
+            Explore Land League
           </Link>
         </div>
       </div>

20848da2412ad62cc9443338a7e21af389dcd7ff
arnavnagpurkar <arnavn.dev@gmail.com>
2024-03-30 09:38:08 +0530

    Solutions and Problems design

--- src/app/landleague/page.js
+++ src/app/landleague/page.js
@@ -2,35 +2,36 @@ import Image from "next/image";
 
 const LandLeague = () => {
   return (
-    <main className='md:max-w-[80vw] mx-auto'>
+    <main className='md:max-w-[80vw] mx-auto min-h-[77vh]'>
       <div className='my-12 mx-5'>
         <h2 className="text-4xl font-semibold my-6">
-          Deforestation and Its Solutions:
+          Deforestation and It&apos;s Solutions:
         </h2>
         <div className="flex flex-wrap">
-          <ul className="flex gap-6 flex-wrap w-full">
-            <li className="flex flex-col gap-2 w-full md:w-auto">
-              <div className="prob-box border p-5">
-                <div className="flex gap-2">
+          <ul className="flex gap-6 flex-wrap w-full mt-5">
+            <li className="flex flex-col gap-2 w-full md:w-96">
+              <div className="prob-box border dark:border-white border-black p-5 rounded-xl hover:scale-105 transition-all">
+                <div className="flex justify-center items-center">
                   <Image
                     src={"/images/co2.png"}
-                    height={20}
-                    width={20}
+                    height={45}
+                    width={45}
                     alt="co2"
+                    className="dark:invert invert-0"
                   />
-                  <h3>
-                    Deforestation emits CO2
-                  </h3>
                 </div>
-                <div className="text">
+                <h3 className="mt-4 text-2xl font-medium text-center">
+                  Deforestation emits CO<span className="text-sm">2</span>
+                </h3>
+                <div className="text-center mt-2 text-[0.95rem] dark:text-zinc-300 text-zinc-700">
                   Deforestation contributes to climate change by releasing stored carbon dioxide into the atmosphere.
                 </div>
               </div>
-              <div className="sol-box border p-5">
-                <h3>
-                  Solution:
+              <div className="sol-box border dark:border-white border-black p-5 rounded-xl hover:scale-105 transition-all">
+                <h3 className="text-center text-xl font-medium">
+                  Solution
                 </h3>
-                <div className="text">
+                <div className="text-center mt-2 text-[0.95rem] dark:text-zinc-300 text-zinc-700">
                   Implementing reforestation and afforestation projects to absorb carbon dioxide and mitigate climate change effects.
                 </div>
               </div>
